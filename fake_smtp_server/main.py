1d4aa3cbd69339bea7075075db642bd62fd86dae
Sébastien LUNDI <slundi@gmail.com>
2022-06-11 10:04:22 +0200

    Check target email

--- fake_smtp_server/main.py
+++ fake_smtp_server/main.py
@@ -1,11 +1,14 @@
-import os
+import os, re
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+email_regex = re.compile(r"[^@]+@[^@]+\.[^@]+") # <account>@<host>.<tld>
 
 @app.get("/send")
 def send_email(to: str, subject: str, body: str):
+    if not email_regex.match(to):
+        raise HTTPException(status_code=400, detail="Invalid email target")
     print("Sending email\n\t\tFrom:   ", os.getenv("from_email"),"\n\t\tTo:     ", to,"\n\t\tSubject:",subject,"\n\t\tBody:   ", body)
     return True
