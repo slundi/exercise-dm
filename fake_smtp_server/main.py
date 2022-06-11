f27e2ad699f497e95dce3b31fae7cf2c5d662066
Sébastien LUNDI <slundi@gmail.com>
2022-06-11 14:10:02 +0200

    Environment vars

--- fake_smtp_server/main.py
+++ fake_smtp_server/main.py
@@ -9,5 +9,5 @@ email_regex = re.compile(r"[^@]+@[^@]+\.[^@]+") # <account>@<host>.<tld>
 def send_email(to: str, subject: str, body: str):
     if not email_regex.match(to):
         raise HTTPException(status_code=400, detail="Invalid email target")
-    print("Sending email\n\t\tFrom:   ", os.getenv("from_email"),"\n\t\tTo:     ", to,"\n\t\tSubject:",subject,"\n\t\tBody:   ", body)
+    print("Sending email\n\t\tFrom:   ", os.getenv("FROM_EMAIL"),"\n\t\tTo:     ", to,"\n\t\tSubject:",subject,"\n\t\tBody:   ", body)
     return True
