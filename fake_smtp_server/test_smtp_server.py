f56385cf710b951b247ed3130796ff849bff25ad
Sébastien LUNDI <slundi@gmail.com>
2022-06-11 19:21:27 +0200

    Split tests

--- fake_smtp_server/test_smtp_server.py
+++ fake_smtp_server/test_smtp_server.py
@@ -5,15 +5,13 @@ from fastapi.testclient import TestClient
 from .main import app
 
 client = TestClient(app)
+os.environ["FROM_EMAIL"] = "test@test.test"
 
-def test_smtp_server():
-    os.environ["FROM_EMAIL"] = "test@test.test"
-    # check OK email
+def test_email_ok():
     response = client.post("/send", data={'to': 'check@test.ts', 'subject': 'test subject', 'body': 'sample text'})
     assert response.status_code == 200
     assert response.json() == True
 
-    # check wrong email
+def test_wrong_email():
     response = client.post("/send", data={'to': 'wrong-email', 'subject': 'test subject', 'body': 'sample text'})
     assert response.status_code == 400
-
