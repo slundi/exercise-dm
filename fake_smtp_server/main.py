b47ab223dd65108308c2889abbfe92bfd0c9c032
Sébastien LUNDI <slundi@gmail.com>
2022-06-11 09:54:09 +0200

    Dirty console alignment

--- fake_smtp_server/main.py
+++ fake_smtp_server/main.py
@@ -7,5 +7,5 @@ app = FastAPI()
 
 @app.get("/send")
 def send_email(to: str, subject: str, body: str):
-    print("Sending email\n\t\tFrom:\t", os.getenv("from_email"),"\n\t\tTo:\t", to,"\n\t\tSubject:\t",subject,"\n\t\tBody:\t:", body)
+    print("Sending email\n\t\tFrom:   ", os.getenv("from_email"),"\n\t\tTo:     ", to,"\n\t\tSubject:",subject,"\n\t\tBody:   ", body)
     return True
